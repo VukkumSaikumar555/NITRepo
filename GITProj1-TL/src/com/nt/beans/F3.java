6d0864fd7977b3133821cf97bdd3353dd8350a20
TL <TL@gmail.com>
2021-03-05 10:36:08 +0530

    dev1-b1 commit

--- GITProj1-TL/src/com/nt/beans/F3.java
+++
@@ -1,7 +0,0 @@
-package com.nt.beans;
-
-public class F3 {
-       public void m1( ) {
-    	   System.out.println("F3.m1()");
-       }
-}
---
+++ GITProj1-TL/src/com/nt/test/F1.java
@@ -0,0 +1,8 @@
+package com.nt.test;
+
+public class F1 {
+	public void x() {
+		System.out.println("F1.x()");
+	}
+
+}
