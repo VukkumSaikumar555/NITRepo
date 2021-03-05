80f8c91a4a1c194437a2f991f1cd80957e127cec
TL <TL@gmail.com>
2021-03-05 12:22:39 +0530

    jhgajfgja

--- GITProj1-TL/src/com/nt/test/F2.java
+++ GITProj1-TL/src/com/nt/test/F2.java
@@ -7,7 +7,13 @@ public class F2 {
    public void m1() {
 	   System.out.println("m1");
    }
+
   public void m3() {
 	  System.out.println("m3");
   }
+
+  public void m2() {
+	  System.out.println("m2");
+  }
+
 }
